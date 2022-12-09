db16ad1a431013546efe2b1e0b20c30190553efa
Felissi <felissistarry@gmail.com>
2022-12-09 22:36:03 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="LoadPyScript">load</button>
+    <py-script>
+      print(window.__NUXT__)
+    </py-script>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isPyScriptLoaded: false
+    }
+  },
+  methods: {
+    LoadPyScript() {
+      const head = document.getElementsByTagName('head')[0]
+      const node = document.createElement('script')
+      node.src = 'https://pyscript.net/latest/pyscript.js'
+      head.appendChild(node)
+      this.isPyScriptLoaded = true
+      this.nextStep()
+    },
+    nextStep() {
+      
+    }
+  }
+}
+</script>
